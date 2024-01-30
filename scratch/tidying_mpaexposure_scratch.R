38c3011881e4045fee9d924d792605a2859b2f39
Niki Sridhar <nikitasridhar12@gmail.com>
2024-01-30 16:46:13 +0530

    IMP: making a lot of changes. gave up on cleaning for loops, but cleaning from this point onwards in script. mainly, removing occurences of historic_varx, midcen_varx, endcen_varx (duplicated) so they are just varx with a period column. and adding functions too.

--- scratch/tidying_mpaexposure_scratch.R
+++ scratch/tidying_mpaexposure_scratch.R
@@ -20,11 +20,11 @@ sum_historic2 <- create_summmary_stats("historic")
 #scratch for simplifying for loop
 
 mpaslist = unique(mpa$File) 
-mpas = rep(NA, 365*363)
-julianday = rep(1:365, 363)
-T_clim = rep(NA, 365*363)
-pH_clim = rep(NA, 365*363)
-DO_clim = rep(NA, 365*363)
+mpas = rep(NA, 365*121)
+julianday = rep(1:365, 121)
+T_clim = rep(NA, 365*121)
+pH_clim = rep(NA, 365*121)
+DO_clim = rep(NA, 365*121)
 
 #Set up a vector of julian day assignment for the 15th of each month and the first and last day of the year
 x_in <- yday(as.Date(c("2000-01-01", "2000-01-15","2000-02-15","2000-03-15","2000-04-15","2000-05-15","2000-06-15",
@@ -56,53 +56,17 @@ for (i in 1:length(mpaslist)){
 historic_tempdata <- data.frame(mpas, julianday,T_clim) %>%
   rename(File = mpas)
 
-#MIDCEN
-mpa_midcen_climatology <- mpa_climatology %>%
-  filter(period == "midcen")
 
-for (i in 1:length(mpaslist)){
-  print(i)
-  d = mpa_midcen_climatology %>% filter(File == mpaslist[i]) %>% select(T_clim)
-  #use a weighted average to get these
-  Dec31 = as.numeric(( ((16/30) * (d[12,2])) + ((14/30) * d[1,2]) ))
-  Jan1 = as.numeric(( ((14/30) * (d[12,2])) + ((16/30) * d[1,2]) ))
-  # a list of y-values of the climatological temp on each of the days in x_in
-  y_in <- c(Jan1, d$T_clim, Dec31)
+
+mpa_climatology_temp <- mpa_climatology %>%
+  mutate(Day = 15)
+
+julian_15 <- mpa %>%
+  filter(Day == 15)
+
+x_out <- rep(NA, 365*363)
+
+
+test <- approx(x = mpa_climatology_temp$T_clim, y = mpa_climatology_temp$Day, xout = x_out )
+
   
-  mod = approx(x = x_in, y = y_in, xout = x_out) 
-  mpas[((i-1)*365+1):(i*365)] <- mpaslist[i] #rep(mpaslist[1], 365)
-  T_clim[((i-1)*365+1):(i*365)] <- mod$y
-}
-
-midcen_tempdata = data.frame(mpas, julianday,T_clim)
-midcen_tempdata <- midcen_tempdata %>%
-  rename(File = mpas)
-
-#ENDCEN
-
-mpa_endcen_climatology <- mpa_climatology %>%
-  filter(period == "endcen")
-
-for (i in 1:length(mpaslist)){
-  print(i)
-  d = mpa_endcen_climatology %>% filter(File == mpaslist[i]) %>% select(T_clim)
-  #use a weighted average to get these
-  Dec31 = as.numeric(( ((16/30) * (d[12,2])) + ((14/30) * d[1,2]) ))
-  Jan1 = as.numeric(( ((14/30) * (d[12,2])) + ((16/30) * d[1,2]) ))
-  # a list of y-values of the climatological temp on each of the days in x_in
-  y_in <- c(Jan1, d$T_clim, Dec31)
-  
-  mod = approx(x = x_in, y = y_in, xout = x_out) 
-  mpas[((i-1)*365+1):(i*365)] <- mpaslist[i] #rep(mpaslist[1], 365)
-  T_clim[((i-1)*365+1):(i*365)] <- mod$y
-}
-
-endcen_tempdata = data.frame(mpas, julianday,T_clim)
-endcen_tempdata <- endcen_tempdata %>%
-  rename(File = mpas)
-
-
-
-
-
-
