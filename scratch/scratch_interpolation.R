667834a5b3dea30a2b740d67468b2769ff8ba20f
Niki Sridhar <nikitasridhar12@gmail.com>
2024-02-07 16:29:57 +0530

    was getting a warning in for loop. resolved - for x_out, changed to every day of year, rather than every day of year EXCLUDING days in x_in (aka 1st last day of year + 15th of every month). checks out with clim data - 15th julian day aligns with clim value.

--- scratch/scratch_interpolation.R
+++ scratch/scratch_interpolation.R
@@ -46,3 +46,68 @@ historic_tempdata_test <- historic_tempdata_test %>%
   rename(interp_temp = T_clim_test)
 
 
+
+interpolate <- function(periodt){
+  
+  mpa_period_climatology <- mpa_climatology %>%
+    filter(period == periodt)
+  
+  for (i in 1:length(mpaslist)){
+    d = mpa_period_climatology %>% filter(File == mpaslist[i]) %>% select(File,Month,T_clim)
+    #use a weighted average. for dec 31, slightly more than half weight to dec, little less than   half weight to jan. for jan 1, slightly more weight to jan, little less weight to dec
+    Dec31 = as.numeric(( ((16/30) * (d[12,3])) + 
+                           ((14/30) * d[1,3]) )) 
+    Jan1 = as.numeric(( ((14/30) * (d[12,3])) + 
+                          ((16/30) * d[1,3]) ))
+    # a list of y-values of the climatological temp on each of the days in x_in
+    y_in <- c(Jan1, d$T_clim, Dec31)
+    
+    mod = approx(x = x_in,  #julian day for the 15th of each month &first/ last day of the year
+                 y = y_in, #temp values per day
+                 xout = x_out) #list of all the days of the year not  in x_in to interpolate to
+    
+    mpas[((i-1)*365+1):(i*365)] <- mpaslist[i] #rep(mpaslist[1], 365)
+    T_clim[((i-1)*365+1):(i*365)] <- mod$y
+  }}
+
+
+#final function - getting a warning: Warning: number of items to replace is not a multiple of replacement length. trying to resolve this. 
+interpolate <- function(periodt)
+  
+  mpa_period_climatology <- mpa_climatology %>%
+    filter(period == "historic")
+  
+  for (i in 1:length(mpaslist)){
+    d = mpa_period_climatology %>% filter(File == mpaslist[i]) %>% select(File,Month,T_clim)
+    #use a weighted average. for dec 31, slightly more than half weight to dec, little less than   half weight to jan. for jan 1, slightly more weight to jan, little less weight to dec
+    Dec31 = as.numeric(( ((16/30) * (d[12,3])) + 
+                           ((14/30) * d[1,3]) )) 
+    Jan1 = as.numeric(( ((14/30) * (d[12,3])) + 
+                          ((16/30) * d[1,3]) ))
+    # a list of y-values of the climatological temp on each of the days in x_in
+    y_in <- c(Jan1, d$T_clim, Dec31)
+    
+    mod = approx(x = x_in,  #julian day for the 15th of each month &first/ last day of the year
+                 y = y_in, #temp values per day in x_in
+                 xout = x_out) #list of all the days of the year not  in x_in to interpolate to
+  
+    #this is where warning starts:
+    mpas[((i-1)*365+1):(i*365)] <- mpaslist[i] #rep(mpaslist[1], 365) 
+    T_clim[((i-1)*365+1):(i*365)] <- mod$y
+  }
+  
+  
+  df <- data.frame(mpas, julianday, T_clim) %>%
+    rename(File = mpas) %>%
+    mutate(period = "historic") %>%
+    rename(interp_temp = T_clim)
+  
+  assign(x = paste("tempdata","historic", sep="_"), value = df, envir = globalenv())
+  
+
+    
+
+
+
+interpolate("historic")  
+
