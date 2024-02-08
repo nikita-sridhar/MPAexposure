cc4a9cae9e859ff9a98f08a1b8c148e3a0644a47
Niki Sridhar <nikitasridhar12@gmail.com>
2024-02-08 13:33:23 +0530

    function and code for anom maps

---
+++ scratch/scratch_anom_map.R
@@ -0,0 +1,89 @@
+#trying to add diff btwn variables as an ifelse statement - haven't fully figured out yet
+
+make_anom_map <- function(mpa_summary_df, sumstat, periodt, diff = FALSE){
+  
+  anom_points <- st_as_sf((mpa_summary_df %>% filter(period == periodt)), 
+                          coords = c("degx","degy"))
+  
+  #calculating diff btwn time points for given sumstat
+  period_diff_anom_points <- st_as_sf((mpa_summary_df %>%
+                                         pivot_wider(names_from = period,
+                                                     values_from = sumstat,
+                                                     id_cols = c(File, degx, degy)) %>%
+                                         mutate(mid_hist_diff = midcen - historic,
+                                                end_hist_diff = endcen - historic,
+                                                end_mid_diff = endcen - midcen)),
+                                      coords = c("degx","degy"))
+  
+  #finding equal interval breaks based on all time periods
+  all_breaks <- classInt::classIntervals(mpa_summary_df[[sumstat]], 10, "equal")
+  
+  all_diff_breaks <- classInt::classIntervals(c(period_diff_anom_points$mid_hist_diff,period_diff_anom_points$end_hist_diff,period_diff_anom_points$end_mid_diff), 10, "equal")
+  
+  if_else(diff = FALSE,
+          
+          #if parameter for diff is TRUE, plot just variable of interest
+          tm_shape(CA_shp) + #basemap
+            tm_fill(col = "#ccebc5") +
+            
+            tm_shape(anom_points) +
+            tm_dots(col = sumstat, size = 0.05,
+                    breaks = all_breaks$brks) +
+            
+            tm_layout(bg.color = "#a6cee3", 
+                      main.title = "Annual # of anomalous heat events",
+                      main.title.position = "center",
+                      main.title.size = .95,
+                      legend.title.size = .9,
+                      legend.text.size = .5),
+          
+          #if parameter for diff is FALSE, plot difference between time periods
+          tm_shape(CA_shp) + #basemap
+            tm_fill(col = "#ccebc5") +
+            
+            tm_shape(anom_points) +
+            tm_dots(col = sumstat, size = 0.05,
+                    breaks = all_breaks$brks) +
+            
+            tm_layout(bg.color = "#a6cee3", 
+                      main.title = "Annual # of anomalous heat events",
+                      main.title.position = "center",
+                      main.title.size = .95,
+                      legend.title.size = .9,
+                      legend.text.size = .5)
+  )
+}
+
+make_anom_map(temp_mpa_summary, "annual_days_abovethresh", "historic")
+make_anom_map(temp_mpa_summary, "annual_days_abovethresh", "historic", TRUE)
+
+
+make_anom_map(temp_mpa_summary, "annual_days_abovethresh", "midcen")
+make_anom_map(temp_mpa_summary, "annual_days_abovethresh", "endcen")
+
+
+
+  anom_points <- st_as_sf((temp_mpa_summary %>% filter(period == "historic")), 
+                          coords = c("degx","degy"))
+  
+  
+  #finding equal interval breaks based on all time periods
+  all_breaks <- classInt::classIntervals(temp_mpa_summary$`Mean num of events in a year`, 10, "equal")
+  
+  tm_shape(CA_shp) + #basemap
+    tm_fill(col = "#ccebc5") +
+    
+    tm_shape(anom_points) +
+    tm_dots(col = "Mean num of events in a year", size = 0.05,
+            breaks = all_breaks$brks) +
+    
+    tm_layout(bg.color = "#a6cee3", 
+              main.title = "title",
+              main.title.position = "center",
+              main.title.size = .8,
+              legend.title.size = .9,
+              legend.text.size = .5)
+  
+
+
+
