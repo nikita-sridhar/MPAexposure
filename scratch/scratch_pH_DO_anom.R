6d87f7cd319259fe4f039ec9d76d788b41fe2095
Niki Sridhar <nikitasridhar12@gmail.com>
2026-08-04 16:44:04 -0800

    final anom analysis

--- scratch/scratch_pH_DO_anom.R
+++ scratch/scratch_pH_DO_anom.R
@@ -152,12 +152,10 @@ events <- mpa %>%
          is_pH_and_DO_low = ifelse(is_pH_low == TRUE & is_DO_low == TRUE, TRUE, FALSE),
          pH_and_DO_event = label_events(is_pH_and_DO_low)) 
 
-pH_DO_event <- function(variable){
 
 event_summary <- events %>%
   
-  ifelse(variable = "pH"){
-  
+
   filter(is_pH_low == TRUE) %>% 
   select(-DO_event, -DO, -is_DO_low, -is_pH_and_DO_low, -pH_and_DO_event) %>%
   group_by(File, pH_event) %>% 
@@ -206,40 +204,100 @@ ph_do_events <- mpa %>%
   group_by(period) %>%
   select(File, Date, Year,NAME, pH, DO, period, degx, degy, julianday) %>%
   mutate(is_pH_low = pH < 7.75,
-         pH_event_id = ifelse(is_pH_low == "FALSE", 0, label_events(is_pH_low)),
+         pH_event_id = ifelse(is_pH_low == "FALSE", NA, label_events(is_pH_low)),
          is_DO_low = DO < 4.6,
-         DO_event_id = ifelse(is_DO_low == "FALSE", 0, label_events(is_DO_low)),
-         is_pH_and_DO_low = ifelse(is_pH_low == TRUE & is_DO_low == TRUE, TRUE, FALSE),
-         pH_and_DO_event_id = label_events(is_pH_and_DO_low)) 
+         DO_event_id = ifelse(is_DO_low == "FALSE", NA, label_events(is_DO_low))) 
 
 #pH event summary
-pH_event_summary <- ph_do_events %>%
-  select(-DO_event_id, -DO, -is_DO_low, -is_pH_and_DO_low, -pH_and_DO_event_id) %>%
-  
-  #event scale (only calculate summary stats when an event took place)
+ph_do_event_summary <- ph_do_events %>%
+
+  #ph events (only calculate summary stats when an event took place)
   group_by(File, pH_event_id, period) %>% 
-  mutate(duration_days = if_else(is_pH_low == "TRUE", n(), NA),
-         duration_days = if_else(duration_days == 1, NA, duration_days), #want 1 day event to register as no event
-         pH_event_id = if_else(duration_days == 0, NA, pH_event_id),#want to just make non events an NA
-         event_mean = if_else(duration_days > 1, mean(pH), NA), 
-         intensity = if_else(duration_days > 1, 7.75 - event_mean, NA),
-         severity = if_else(duration_days > 1, intensity*duration_days, NA)) %>%
+  mutate(pH_duration_days = if_else(is_pH_low == "TRUE", n(), NA),
+         pH_duration_days = if_else(pH_duration_days == 1, NA, pH_duration_days), #want 1 day event to register as no event
+         pH_event_id = if_else(pH_duration_days == 0, NA, pH_event_id),#want to just make non events a 0 (incl 2 day events w duration days)
+         pH_event_mean = if_else(pH_duration_days > 1, mean(pH, na.rm = TRUE), NA)) %>%
+  ungroup() %>%
+  
+  #do events
+  group_by(File, DO_event_id, period) %>% 
+  mutate(DO_duration_days = if_else(is_DO_low == "TRUE", n(), NA),
+         DO_duration_days = if_else(DO_duration_days == 1, NA, DO_duration_days), #want 1 day event to register as no event
+         DO_event_id = if_else(DO_duration_days == 0, NA, DO_event_id),#want to just make non events an NA
+         DO_event_mean = if_else(DO_duration_days > 1, mean(DO, na.rm = TRUE), NA)) %>%
   ungroup() 
 
-
 #since an event can span multiple years, annual sum is n() in that year (not duration days since that can show duration spanning multiple years)
-annual_event_counts <- pH_event_summary %>%
+ph_do_annual_event_counts <- ph_do_event_summary %>%
+  #per mpa per year
   group_by(File, Year, period) %>%
   mutate(annual_days_below_pH_thresh = sum(!is.na(pH_event_id)),
-         annual_avg_pH_belowthresh = mean(pH, na.rm = TRUE)) %>%
+         annual_avg_pH_belowthresh = mean(pH_event_mean, na.rm = TRUE),
+         annual_days_below_DO_thresh = sum(!is.na(DO_event_id)),
+         annual_avg_DO_belowthresh = mean(DO_event_mean, na.rm = TRUE)) %>%
   ungroup() %>%
-  #mpa summary
+  #per mpa
   group_by(File, period) %>%
   mutate(avg_annual_days_below_pH_thresh = mean(annual_days_below_pH_thresh, na.rm=TRUE),
-         avg_annual_ph_belowthresh = mean(annual_avg_pH_belowthresh, na.rm= TRUE)) %>%
+         avg_annual_ph_belowthresh = mean(annual_avg_pH_belowthresh, na.rm= TRUE),
+         avg_annual_days_below_DO_thresh = mean(annual_days_below_DO_thresh, na.rm=TRUE),
+         avg_annual_DO_belowthresh = mean(annual_avg_DO_belowthresh, na.rm= TRUE)) %>%
   ungroup() %>%
+  distinct(File, NAME, degx, degy, period, avg_annual_days_below_pH_thresh, 
+           avg_annual_ph_belowthresh, avg_annual_days_below_DO_thresh, avg_annual_DO_belowthresh)
 
-distinct(File, NAME, degx, degy, period, avg_annual_days_below_pH_thresh, avg_annual_ph_belowthresh) 
+
+#temperature events: 2+ days where daily temp > (historical climatology's temp for that month) + 2* (historical climatology SD)
+
+temp_events <- mpa %>%
+  
+  #first calculating reference historic climatology
+  filter(period == "historic") %>%
+  group_by(File, Month) %>%
+  summarise(histclim_Temp_mean = mean(Temp), 
+            histclim_Temp_sd = sd(Temp)) %>% #this sd is temp over all days in Jan - rather than average of each jan's SD - does it make a difference? 
+  #bringing back to scale of julian days and all time periods
+  merge(mpa, by = c("File", "Month")) %>%
+  select(File, Date, Year, NAME, Temp, period, degx, degy, julianday, histclim_Temp_mean, histclim_Temp_sd) %>%
+  
+  #labelling events
+  group_by(period) %>%
+  mutate(is_temp_high = Temp > (histclim_Temp_mean + 2*histclim_Temp_sd),
+         temp_event_id = ifelse(is_temp_high == "FALSE", NA, label_events(is_temp_high))) %>%
+  select(-histclim_Temp_mean, -histclim_Temp_sd) %>%
+         
+  #temp event summary 
+  group_by(File, temp_event_id, period) %>% 
+  mutate(temp_duration_days = if_else(is_temp_high == "TRUE", n(), NA),
+         temp_duration_days = if_else(temp_duration_days == 1, NA, temp_duration_days), #want 1 day event to register as no event
+         temp_event_id = if_else(temp_duration_days == 0, NA, temp_event_id),#want to just make non events an NA
+         temp_event_mean = if_else(temp_duration_days > 1, mean(Temp, na.rm = TRUE), NA)) %>%
+  ungroup() 
+
+#putting it all together
+annual_events <- temp_events %>%
+  #first finding annual averages for temp
+  #per mpa per year
+  group_by(File, Year, period) %>%
+  mutate(annual_days_above_temp_thresh = sum(!is.na(temp_event_id)),
+         annual_avg_temp_abovethresh = mean(temp_event_mean, na.rm = TRUE)) %>%
+  ungroup() %>%
+  #per mpa
+  group_by(File, period) %>%
+  summarise(avg_annual_days_above_temp_thresh = mean(annual_days_above_temp_thresh, na.rm=TRUE),
+         avg_annual_temp_belowthresh = mean(annual_avg_temp_abovethresh, na.rm= TRUE)) %>%
+  ungroup() %>%
+  right_join(ph_do_annual_event_counts, by = c("File", "period"))
+
+
+
+  
+
+
+  
+  
+  
+
 
 
 
